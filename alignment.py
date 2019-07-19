c307abc2bee1897a69ebebe49154e691512e4f9d
michael <emvalenc@go.olemiss.edu>
2019-07-19 14:48:16 -0500

    third derivatives development

--- alignment.py
+++ alignment.py
@@ -11,116 +11,61 @@ differential = 0.005
 nn = 1
 
 size = raw_data.shape
-num_of_jobs = raw_data.size * 2
+num_of_jobs = raw_data.size
 
-count_global = 0
+third_list = []
 
-np.set_printoptions(suppress=True,
-   formatter={'float_kind':'{:20.10f}'.format})
+def determine_atom(row,atom_number,item):
 
-zero_array = np.zeros((27,3))
+    if int(third_array[row, atom_number, item]) % 3 == 0:
+        third_array[row,atom_number,item] = 0
+    elif int(third_array[row, atom_number,item]) % 3 == 1:
+        third_array[row,atom_number,item] = 1
+    elif int(third_array[row,atom_number,item]) % 3 == 2:
+        third_array[row,atom_number,item] = 2
 
-zero_size = zero_array.shape
+def determine_coordinate(row,coordinate_number,item):
+    if int(third_array[row, coordinate_number,item]) // 3 == 0:
+        third_array[row,coordinate_number,item] = 0
+    elif int(third_array[row, coordinate_number,item]) // 3 == 1:
+        third_array[row,coordinate_number,item] = 1
+    elif int(third_array[row,coordinate_number,item]) // 3 == 2:
+        third_array[row,coordinate_number,item] = 2
 
-item_count = 0
-doubles = 0
-x_list = 0
+#You can also turn this into X,Y,Z instead of 0,1,2, but I think it's easier this way since you can use all ints.
 
-print(a[0])
-print(len(a))
-print(len(b))
+for i in range(num_of_jobs):
+    for j in range(num_of_jobs):
+        for k in range(num_of_jobs):
+            if j <= i and k <= j:
+                third_list.append(((i,i),(j,j),(k,k)))
 
-print(zero_size)
+third_array = np.asarray(third_list)
 
-#New Ideas:
-number_of_points = (size[0])**4
-geom_list = [0,0,0,0]
+third_array_shape = third_array.shape
+print(third_array_shape)
 
-zero_array = zero_array.astype('object')
+third_array = third_array.astype('object')
 
-for i in range(27):
-    for j in range(3):
-        zero_array[i,j] = [0,0,0,0]
+print(third_array[0])
 
-array = []
-for atom1 in range(3):
-    coordinate_list1 = []
-    for coordinate1 in range(3):
-        atom_list1 = []
-        for atom2 in range(3):
-            coordinate_list2 = []
-            for coordinate2 in range(3):
-                coordinate_list2.append(coordinate2)
-            atom_list1.append(coordinate_list2)
-        coordinate_list1.append(atom_list1)
-    array.append(coordinate_list1)
+for row in range(third_array_shape[0]):
+    for col in range(third_array_shape[1]):
+        pair = 0
+        if pair == 0:
+            determine_atom(row,col,pair)
+    for col in range(third_array_shape[1]):
+        pair = 1
+        if pair == 1:
+            third_array[row,col,1] = int(third_array[row,col,1])
+            determine_coordinate(row,col,1)
 
-array = np.asarray(array)
-
-shapefour = array.shape
-print(shapefour)
-
-def convert(list):
-    s = [str(i) for i in list]
-    res = int("".join(s))
-    return(res)
-
-def manipulate_geometry_first(a1,c1,a2,c2):
-    print("no")
-
-def manipulate_geometry_second(a1,c1,a2,c2):
-    if a1 == a2 and c1 == c2:
-        raw_data[a1,c1] = raw_data[a1,c1] + differential*2
-        pos_dif = np.column_stack((labels,raw_data))
-        print(pos_dif)
-
-        raw_data[a1,c1] = raw_data[a1,c1] - differential*2
-        neg_dif = np.column_stack((labels,raw_data))
-        print(neg_dif)
-    else:
-        raw_data[a1,c1] = raw_data[a1,c1] + differential
+print(third_array)
 
 
-for i in range(shapefour[0]):
-    for j in range(shapefour[1]):
-        Found = False
-        for k in range(shapefour[2]):
-            for l in range(shapefour[3]):
-                if i == k and j == l:
-                    array[i,j,k,l] = 7
-                    value = (i,j,k,l)
-                    res = convert(value)
-                    Found = True
-                elif Found:
-                    comparison_list = (i,j,k,l)
-                    comparevalue = convert(comparison_list)
-                    if comparevalue > res:
-                        array[i,j,k,l] = 777
-                        print(comparevalue, res)
-                    else:
-                        array[i,j,k,l] = 0
-                else:
-                    array[i,j,k,l] = 0
-
-print(array)
-
-second_energy_list_a = [0.00037726010759797646, 1.3377116501089859, 0.7794059200705306, 0.0004889599836133129, 2.4523686801103395, 1.6416705300059675, 0.0003772402124013752, 1.3377116501089859, 0.7794059200705306]
-second_energy_list_b = [0.0, 0.0, -0.000109899929157109, 0.0, 0.0, -9.700045211502584e-06, 0.0, 0.0, 0.839248540103199, -1.2260569000943633, -0.7274316999428265, -0.11139000008597577, -0.1118235799424383, -0.8207080598765515, 0.041567169972722695, 0.0, 0.0, 0.0, 0.0, -0.000109899929157109, 0.0, 0.0, -0.951072299955058, 0.0, -1.226056890146765, 0.951072299955058, -0.8207080598765515, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.1118235799424383, 0.7274316999428265, -0.8392485300134922]
-
-a = []
-b = []
-
-np.asarray(second_energy_list_a)
-np.asarray(second_energy_list_b)
-
-for n in second_energy_list_a:
-    a.append(n * (0.529177208)**2)
-
-for n in second_energy_list_b:
-    b.append(n * (0.529177208)**2)
-
-print(a)
-print(b)
-
-x_repeat = []
-y_repeat = []
+def third_geometries():
+    for row in range(third_array_shape[0]):
+        for col in range(0,third_array_shape[1],2):
+            third_array[row,col] = third_atom
+            third_array[row,col+1] = third_coordinate
+            raw_data
